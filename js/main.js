a3b690d2a9ffe4a2d053d6636d9e0bc7b4b685a3
Viktor Kukurba <victor.kukurba@sethq.com>
2017-03-06 00:19:49 +0200

    Update bookmark render

--- js/main.js
+++ js/main.js
@@ -7,20 +7,19 @@ function block_show(){
 	 $(".well").animate({height: 'show'}, 500); 
 	}
 //save Bookmark
-function saveBookmark(e){
+function saveBookmark(e) {
 //Get form values"
-var siteName=document.getElementById("siteName").value;
-var siteUrl=document.getElementById("siteUrl").value;
-if(!validateForm(siteName,siteUrl)){
+  var siteName = document.getElementById("siteName").value;
+  var siteUrl = document.getElementById("siteUrl").value;
+  if (!validateForm(siteName, siteUrl)) {
 	return false;
-}
-var bookmark={
+  }
+  var bookmark = {
 	name: siteName,
 	url: siteUrl
-}
+  };
 
-
- //Local Storage Test
+  //Local Storage Test
 // localStorage.setItem('test','Hello, World!');
 // console.log(localStorage.getItem('test'));
 // console.log(localStorage.removeItem('test'));
@@ -29,28 +28,29 @@ var bookmark={
 // console.log(bookmark);
 
 //Test if bookmarks is null
-if(localStorage.getItem('bookmarks')===null){
+  if (localStorage.getItem('bookmarks') === null) {
 //innit array
-var bookmarks=[];
+	var bookmarks = [];
 //add to array
-bookmarks.push(bookmark);
+	bookmarks.push(bookmark);
 //set to local storage
-localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-} else {
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
 //Get bookmarks from LocalStorage
-var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 //Add boomark to array
-bookmarks.push(bookmark);
+	bookmarks.push(bookmark);
 // Re-set back to LocalStorage
-localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
-}
+  }
 //clear form
-document.getElementById('myForm').reset();
+  document.getElementById('myForm').reset();
 //re-fetch bookmarks
-fetchBookmarks();
+//  fetchBookmarks();
+  addBookmark(bookmark);
 //prevent form from submitting
-e.preventDefault();
+  e.preventDefault();
 }
 //Delete Bookmark
 function deleteBookmark(url){
@@ -69,8 +69,7 @@ localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 fetchBookmarks();
 }
 //Fetch bookmarks
-function fetchBookmarks(){
-
+function fetchBookmarks() {
 //Get bookmarks from LocalStorage
 	var bookmarks= JSON.parse(localStorage.getItem('bookmarks'));
 
@@ -79,17 +78,29 @@ var bookmarksResults=document.getElementById("bookmarksResults");
 
 //Build output
 bookmarksResults.innerHTML="";
+  /**
+   * 7. Avoid DOM Manipulation (Better performance to have less dom manipulation)
+   * @link https://code.tutsplus.com/tutorials/10-ways-to-instantly-increase-your-jquery-performance--net-5551
+   */
+   var htmlString = "";
  for(var i=0; i<bookmarks.length; i++){
- 	var name=bookmarks[i].name;
- 	var url=bookmarks[i].url;
- 	bookmarksResults.innerHTML+='<div id="well" class="well" style="display: none;">'+
- 	                            '<h3>'+name+
- 	                            '<a class="btn btn-default" target="_blank" href="'+url+'">GO</a>'+
- 	                            '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Del</a>'
- 	                            '</h3>'+
- 	                            '</div>'; 	                           
+   htmlString += renderBookmark(bookmarks[i]);
  }
+  bookmarksResults.innerHTML = htmlString;
+  block_show();
 }
+
+/**
+ * Adds bookmark to bookmarks list.
+ * @param {{url: string, name: string}} bookmark Bookmark data.
+ */
+function addBookmark(bookmark) {
+  var block = document.createElement('DIV');
+  block.innerHTML = renderBookmark(bookmark);
+  document.getElementById("bookmarksResults").appendChild(block.firstChild);
+  block_show();
+}
+
 //Validate form
 function validateForm(siteName,siteUrl) {
   if (!siteName || !siteUrl) {
@@ -104,6 +115,30 @@ function validateForm(siteName,siteUrl) {
   }
   return true;
 
+}
+
+/**
+ * Add event listener on document load to render saved bookmarks.
+ * @link https://learn.jquery.com/using-jquery-core/document-ready/
+ */
+$(document).on('ready', fetchBookmarks);
+
+/**
+ * Gets bookmark html string
+ * @param {{url: string, name: string}} bookmark Bookmark data.
+ * @return {string} Bookmark html string.
+ */
+function renderBookmark(bookmark) {
+  /* TODO: remove 'id' or make it unique */
+  /** TODO: remove onclick="deleteBookmark" please use event delegation
+   * @link https://davidwalsh.name/event-delegate
+   */
+   return '<div id="well" class="well" style="display: none">'+
+  '<h3>'+bookmark.name+
+  '<a class="btn btn-default" target="_blank" href="'+bookmark.url+'">GO</a>'+
+  '<a onclick="deleteBookmark(\''+bookmark.url+'\')" class="btn btn-danger" href="#">Del</a>' +
+  '</h3>'+
+  '</div>';
 }
 
 
